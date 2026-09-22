09f9104f77ce616a0dedbe504cea107b4b0ae26f
cmandaman2011 <camerondlindsey11@gmail.com>
2026-09-22 14:40:04 -0500

    Add one song
    Added another song to "SONGS" list to change the total amount of songs to 1 more.

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Workin Day and Night", "Micheal Jackson")
 ]
 
 def print_playlist(songs):
